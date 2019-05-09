aba8a1d816959b1398a140a05b3fead134eb8776
darom <vo0a195@gmail.com>
2019-05-09 20:13:30 +0900

    [Add] 업로드 결과를 저장하도록 함
    이미지를 upload 하면 화면 아래에 추가됨.

--- flask-tutorial/hello.py
+++ flask-tutorial/hello.py
@@ -1,37 +1,40 @@
-﻿
-from flask import Flask, send_from_directory, request, json, redirect
+﻿from flask import Flask, send_from_directory, request, json, redirect
 import datetime
 
 image_dir = "images"
+log_dir = "logs/upload-{0}.log".format(datetime.datetime.now())
 
 app = Flask(__name__)
 
+def write_log(result, path):
+	with open(log_dir, 'a+') as log_file:
+		log_content = "{0}|{1}|{2}\n".format(datetime.datetime.now(), result, path) 
+		log_file.write(log_content)
+		
 @app.route('/')
 def hello_world():
 	return send_from_directory(directory="",filename="index.html")
 
 @app.route("/upload", methods=['POST'])
-def updoad():
+def upload():
+	if "file" not in request.files:
+		result = "Cannot find file"
+		write_log(result, None)
+		return "", 400
+	
 	image_file = request.files["file"]
+	
+	if image_file.filename == "":
+		result = "File name is empty"
+		write_log(result, None)
+		return "", 400
+	
 	path = "{0}/{1}".format(image_dir, image_file.filename)
 	image_file.save(path)
-	history()
-	return json.dumps({"path":path})
+	
+	write_log("Success", path)
+	return json.dumps({"path":path}), 200
 
 @app.route("/images/<name>")
 def image(name):
 	return send_from_directory(directory="image_dir", filename=name)
-	
-@app.route("/history")
-def history():
-	now = datetime.datetime.now()
-	fw = open('/workspace/Tutorial1/history/%s.txt' %now, 'wt', encoding="utf-8")
-	fw.write('업로드된 날짜 : %s' %now)
-	fw.close()
-	
-	fr = open('/workspace/Tutorial1/history/%s.txt' %now, 'rt', encoding="utf-8")
-	text = fr.read()
-	print(text)
-	fr.close()
-	return
-

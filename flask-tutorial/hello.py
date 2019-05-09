0bfd04d642ed843caad00d4d2172c38132cdcf7a
darom <vo0a195@gmail.com>
2019-05-09 19:14:54 +0900

    [Add] 이미지 저장, 보여주기, 이미지 저장 시간 기록
    이미지 파일을 저장하고 이미지가 저장된 시간을 text파일로 저장한다.

---
+++ flask-tutorial/hello.py
@@ -0,0 +1,33 @@
+from flask import Flask, send_from_directory, request, json, redirect
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return send_from_directory(directory="",filename="index.html")
+
+@app.route("/upload", methods=['POST'])
+def updoad():
+	f = request.files["file"]
+	path = "images/" + f.filename
+	f.save(path)
+	history()
+	return json.dumps({"path":path})
+
+@app.route("/images/<name>")
+def image(name):
+	return send_from_directory(directory="images", filename=name)
+	
+@app.route("/history")
+def history():
+	now = datetime.datetime.now()
+	fw = open('/workspace/Tutorial1/history/%s.txt' %now, 'wt', encoding="utf-8")
+	fw.write('업로드 된 시간 : %s' %now)
+	fw.close()
+	
+	fr = open('/workspace/Tutorial1/history/%s.txt' %now, 'rt', encoding="utf-8")
+	text = fr.read()
+	print(text)
+	fr.close()
+	return

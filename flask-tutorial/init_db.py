2b8004e32a5977ea4476f202a1f6d8ac98ce21b8
darom <vo0a195@gmail.com>
2019-05-09 22:18:43 +0900

    [Add] 필요한 경우 테이블을 생성하는 코드

---
+++ flask-tutorial/init_db.py
@@ -0,0 +1,10 @@
+﻿import sqlite3
+
+database_file = 'upload_result.db'
+
+# IF NOT EXISTS 구문을 사용하실 경우 CREATE DATABASE 구문을 사용해 만들고자 하는 데이터베이스가  
+# 이미 존재할 경우 발생할 수 있는 오류를 피할 수 있습니다.
+create_query = "CREATE TABLE IF NOT EXISTS upload_result(date TEXT, result TEXT, path TEXT)"
+with sqlite3.connect(database_file) as conn:
+    c = conn.cursor()
+    c.execute(create_query)
